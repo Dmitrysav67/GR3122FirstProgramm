ee4b78e7c3ae2842d84de480bb874e7010a71f59
Dmitry Savvin <savvin.d67@mail.ru>
2022-10-01 20:23:47 +0300

    add Sem9

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,30 @@
+﻿//------------------------------------------------------------------
+// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+//-------------------------------------------------------------------
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// Сумма натуральных элементов в промежутке от M до N.
+int SumRecMN(int m, int n)
+{
+    if (m >= n) return n;
+    return m + RecMN(m + 1, n);
+}
+
+int m = ReadData("Введите число m: ");
+int n = ReadData("Введите число n: ");
+
+int result = SumRecMN(m, n);
+PrintResult(result);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,34 @@
+﻿//------------------------------------------------------------------
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью 
+// рекурсии. Даны два неотрицательных числа m и n.
+//-------------------------------------------------------------------
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+//вычисления функции Аккермана
+int Akkerman(int m, int n)
+{
+    
+    if (m == 0) return n + 1;
+    if (n == 0) return Akkerman(m - 1, 1);
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+
+}
+
+
+int m = ReadData("Введите число: ");
+int n = ReadData("Введите число: ");
+
+int result = Akkerman(m, n);
+PrintResult(result);
